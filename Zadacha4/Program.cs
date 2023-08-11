d262da911b980dc7647ae2359f5c307b39f6ebe9
«Matvey <mymayl@inbox.ru>
2023-08-12 00:10:47 +0500

    Zad6

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+int i = 0;
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+    System.Console.WriteLine($"{i} ");
+    i = i + 1;
+    }
+    else
+    {
+        i = i + 1;
+    }
+}
