c4be932c21b3495ed4131fca57afb2bf84b42800
«Matvey <mymayl@inbox.ru>
2023-08-11 19:28:31 +0500

    Zad2

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2)
+{
+    Console.WriteLine("Первое число равно второму");
+}
+else if (num1 > num2)
+{
+    Console.WriteLine("Первое число большее, а второе меньшее ");
+}
+else 
+{
+    Console.WriteLine("Второе число большее, а первое меньшее ");
+}
