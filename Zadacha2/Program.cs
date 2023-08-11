4dcc756bc6cb06dd4c35f05c90556789a6f24965
«Matvey <mymayl@inbox.ru>
2023-08-11 20:13:56 +0500

    Zad4

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (num2 > max)
+{
+    max =num2;
+}
+else
+{
+    max = num1;
+}
+ if (num3 > max)
+{
+    max = num3;
+}
+
+System.Console.WriteLine($"Максимальным числом является {max}");
